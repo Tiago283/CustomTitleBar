0cb36988c49201cb1927470816756e22b6439d9f
Tiago283 <tiagogomesdasilva283@gmail.com>
2025-01-10 11:09:52 -0300

    Update

--- src/main.py
+++ src/main.py
@@ -2,15 +2,45 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    #! Important Configurations
     page.window.title_bar_buttons_hidden = True
     page.window.title_bar_hidden = True
 
-    custom_bar = ft.AppBar(
-        title=ft.Text("Custom Title Bar") ## Window Title
+    #? Padding Optional
+    page.padding = 2
+
+    #! Defaut Actions
+    def maximize(e):
+        page.window.maximized = not page.window.maximized
+        page.update()
+
+    def close(e):
+        page.window.destroy()
+
+    ## Title Bar
+    title_bar = ft.Row(
+        [
+            ft.Row(
+                [
+                    ft.Image('icon.png', width=25, height=25), ## Icon
+                    ft.Text("Custom Title Bar"), ## Title
+                ]
+            ), ## Leading
+            ft.Row(
+                [
+                    ft.IconButton(ft.Icons.SQUARE_OUTLINED, on_click=maximize), ## Maximize button
+                    ft.IconButton(ft.Icons.CLOSE, on_click=close) ## Close button
+                ]
+            ), ## Actions
+        ],
+        ft.MainAxisAlignment.SPACE_BETWEEN, ## Title Bar Vertical
+        ft.CrossAxisAlignment.CENTER, ## Title Bar Horizontal
     )
 
-    page.appbar = custom_bar
+    ## Add the Custom Title Bar
+    page.add(ft.WindowDragArea(title_bar))
     page.update()
 
 
-ft.app(main)
+if __name__ == '__main__':
+    ft.app(main, assets_dir='assets')
