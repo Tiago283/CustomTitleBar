2881827e2a9effc31cea58f8e5ef9724e9c08ad9
Tiago283 <tiagogomesdasilva283@gmail.com>
2025-01-11 13:49:47 -0300

    Update

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
 import flet as ft
 
 
+#* Custom Controls
+class TitleBar(ft.WindowDragArea):
+    def __init__(self, actions: list | None = None):
+        self.actions = actions
+        self.icon = ft.Image('icon.png', width=25, height=25)
+        self.title = ft.Text("Flet")
+        self.bar = ft.Row([
+            ft.Row([self.icon, self.title], ft.MainAxisAlignment.START),
+            ft.Row(self.actions, ft.MainAxisAlignment.END)
+        ], ft.MainAxisAlignment.SPACE_BETWEEN, ft.CrossAxisAlignment.CENTER)
+        super().__init__(content=self.bar)
+
+
 def main(page: ft.Page):
     #! Important Configurations
     page.window.title_bar_buttons_hidden = True
     page.window.title_bar_hidden = True
+    page.padding = 2 #? Padding Optional
 
-    #? Padding Optional
-    page.padding = 2
-
-    #! Defaut Actions
     def minimize(e):
         page.window.minimized = not page.window.minimized
         page.update()
@@ -21,29 +31,13 @@ def main(page: ft.Page):
     def close(e):
         page.window.destroy()
 
-    ## Title Bar
-    title_bar = ft.Row(
-        [
-            ft.Row(
-                [
-                    ft.Image('icon.png', width=25, height=25), ## Icon
-                    ft.Text("Custom Title Bar"), ## Title
-                ]
-            ), ## Leading
-            ft.Row(
-                [
-                    ft.IconButton(ft.Icons.REMOVE, on_click=minimize), ## Minimize button
-                    ft.IconButton(ft.Icons.SQUARE_OUTLINED, on_click=maximize), ## Maximize button
-                    ft.IconButton(ft.Icons.CLOSE, on_click=close) ## Close button
-                ]
-            ), ## Actions
-        ],
-        ft.MainAxisAlignment.SPACE_BETWEEN, ## Title Bar Vertical
-        ft.CrossAxisAlignment.CENTER, ## Title Bar Horizontal
-    )
-
     ## Add the Custom Title Bar
-    page.add(ft.WindowDragArea(title_bar))
+    title_bar = TitleBar([
+        ft.IconButton(ft.Icons.REMOVE, on_click=minimize),
+        ft.IconButton(ft.Icons.SQUARE_OUTLINED, on_click=maximize),
+        ft.IconButton(ft.Icons.CLOSE, on_click=close)
+    ])
+    page.add(title_bar)
     page.update()
 
 
